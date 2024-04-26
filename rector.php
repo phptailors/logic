bd1155d23d5a561cd4a5953b7592edab18dd3e3b
Paweł Tomulik <pawel@tomulik.pl>
2024-04-26 14:38:32 +0200

    rector: ensure static data providers (PHPUnit)

--- rector.php
+++ rector.php
@@ -3,12 +3,14 @@
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DataProviderAnnotationToAttributeRector;
 use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\CoversAnnotationWithValueToAttributeRector;
+use Rector\PHPUnit\PHPUnit100\Rector\Class_\StaticDataProviderClassMethodRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/tests/src/',
     ])
     ->withRules([
+        StaticDataProviderClassMethodRector::class,
         CoversAnnotationWithValueToAttributeRector::class,
         DataProviderAnnotationToAttributeRector::class,
     ])
