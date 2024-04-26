0b297143e4d7bfda05457efdcdf6952015c2b9a2
Paweł Tomulik <pawel@tomulik.pl>
2024-04-26 09:39:02 +0200

    install rector

---
+++ rector.php
@@ -0,0 +1,15 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DataProviderAnnotationToAttributeRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\CoversAnnotationWithValueToAttributeRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/tests/src/',
+    ])
+    ->withRules([
+        CoversAnnotationWithValueToAttributeRector::class,
+        DataProviderAnnotationToAttributeRector::class,
+    ])
+;
