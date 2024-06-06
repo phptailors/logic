f11199bd9919791956ed911889d07e7e3d44a549
Paweł Tomulik <pawel@tomulik.pl>
2024-06-07 01:08:06 +0200

    implemented "!==" predicate

---
+++ tests/src/Predicates/NotSameTest.php
@@ -0,0 +1,108 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of phptailors/logic.
+ *
+ * Copyright (c) Paweł Tomulik <pawel@tomulik.pl>
+ *
+ * View the LICENSE file for full copyright and license information.
+ */
+
+namespace Tailors\Logic\Predicates;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\UsesMethod;
+use PHPUnit\Framework\TestCase;
+use Tailors\Logic\AbstractFunctorExpression;
+use Tailors\Logic\InfixNotationTrait;
+use Tailors\Logic\TermInterface;
+use Tailors\PHPUnit\ExtendsClassTrait;
+use Tailors\PHPUnit\UsesTraitTrait;
+
+/**
+ * @author Paweł Tomulik <pawel@tomulik.pl>
+ *
+ * @psalm-suppress MissingThrowsDocblock
+ *
+ * @internal
+ */
+#[CoversClass(NotSame::class)]
+#[UsesMethod(AbstractPredicate::class, 'apply')]
+#[UsesMethod(AbstractFunctorExpression::class, '__construct')]
+#[UsesMethod(PredicateFormula::class, '__construct')]
+final class NotSameTest extends TestCase
+{
+    use ExtendsClassTrait;
+    use UsesTraitTrait;
+
+    #[\Override]
+    public function setUp(): void
+    {
+        // Without setUp() we get MissingConstructor error from psalm
+    }
+
+    public function testExtendsAbstractPredicate(): void
+    {
+        $this->assertExtendsClass(AbstractPredicate::class, NotSame::class);
+    }
+
+    public function testUsesInfixNotationTrait(): void
+    {
+        $this->assertUsesTrait(InfixNotationTrait::class, NotSame::class);
+    }
+
+    public function testUsesBinaryPredicateTrait(): void
+    {
+        $this->assertUsesTrait(BinaryPredicateTrait::class, NotSame::class);
+    }
+
+    public function testSymbolReturnsStrictEqualSign(): void
+    {
+        $sum = new NotSame();
+        $this->assertSame('!==', $sum->symbol());
+    }
+
+    public function testWithReturnsPredicateFormula(): void
+    {
+        $t1 = $this->getMockBuilder(TermInterface::class)->getMock();
+        $t2 = $this->getMockBuilder(TermInterface::class)->getMock();
+        $this->assertInstanceOf(PredicateFormula::class, (new NotSame())->with($t1, $t2));
+    }
+
+    /**
+     * @psalm-return array<array{0:mixed,1:list}>
+     */
+    public static function providerApplyReturnsComparisonResult(): array
+    {
+        return [
+            [true, []],
+            [true, [1]],
+            [true, [1, 2]],
+            [false, [2, 2]],
+            [true, [2, '2']],
+            [true, ['2', 2]],
+            [false, [null, null]],
+            [true, [false, null]],
+            [true, [null, false]],
+            [true, ['', null]],
+            [true, [null, '']],
+            [true, [0, false]],
+            [true, [false, 0]],
+        ];
+    }
+
+    /**
+     * @psalm-param list $arguments
+     */
+    #[DataProvider('providerApplyReturnsComparisonResult')]
+    public function testApplyReturnsComparisonResults(mixed $result, array $arguments): void
+    {
+        $this->assertSame($result, (new NotSame())->apply(...$arguments));
+    }
+
+    public function testPrecedenceReturnValue(): void
+    {
+        $this->assertSame(11, (new NotSame())->precedence());
+    }
+}
